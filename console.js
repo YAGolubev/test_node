8879d03c55bae3f6409261fb171a56ffa671c784
YAGolubev <yagolubev@inbox.ru>
2016-02-28 02:39:14 +0300

    process

--- console.js
+++ console.js
@@ -16,3 +16,14 @@ console.log('i like cake and pies'.underline.red) // outputs red underlined text
 console.log('inverse the color'.inverse); // inverses the color 
 console.log('OMG Rainbows!'.rainbow); // rainbow 
 console.log('Run the trap'.trap); // Drops the bass 
+//-------------------------------------------------
+//node console.js -a beep -b boop
+var argv = process.argv.slice(2);
+console.log(argv);
+
+//node console.js -x 3 -y 4 -n5 -abc --beep=boop goo bar baz
+var argv = require('minimist')(process.argv.slice(2));
+console.dir(argv);
+
+var argv = process.env;
+console.log(argv);
